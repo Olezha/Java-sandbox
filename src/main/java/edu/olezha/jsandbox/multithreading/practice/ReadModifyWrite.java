fe77726bca20717d2c1db81ed0fde1b4a89df0a4
Oleh <oleh.shklyar@gmail.com>
2019-08-03 22:35:19 +0300

    40

---
+++ src/main/java/edu/olezha/jsandbox/multithreading/practice/ReadModifyWrite.java
@@ -0,0 +1,33 @@
+package edu.olezha.jsandbox.multithreading.practice;
+
+public class ReadModifyWrite {
+
+    private static int i;
+    private static int j;
+    private static final Object lock = new Object();
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            synchronized (lock) {
+                for (int i = 0; i < 500_000; i++) {
+                    ReadModifyWrite.i++;
+                    incrementJ();
+                }
+            }
+        }).start();
+
+        for (int i = 0; i < 500_000; i++) {
+            ReadModifyWrite.i++;
+            incrementJ();
+        }
+
+        synchronized (lock) {
+            System.out.println("i: " + i + System.lineSeparator() + "j: " + j);
+        }
+    }
+
+    private static synchronized void incrementJ() {
+        j++;
+    }
+}
