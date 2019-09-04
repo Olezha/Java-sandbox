1d9a32a211604129463a01edf03d86c6d0bafedf
Oleh Shklyar <oleh.shklyar@redwerk.com>
2019-09-04 20:19:51 +0300

    274

---
+++ src/main/java/edu/olezha/jsandbox/multithreading/concurrent/RNG.java
@@ -0,0 +1,38 @@
+package edu.olezha.jsandbox.multithreading.concurrent;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RNG {
+
+    public static void main(String[] args) {
+        System.out.println(xorShift((int) System.nanoTime()));
+        System.out.println(xorShift((int) System.nanoTime()));
+        System.out.println(xorShift((int) System.nanoTime()));
+        System.out.println(xorShift((int) System.nanoTime()));
+
+        System.out.println(xorShift(new Object().hashCode()));
+        System.out.println(xorShift(new Object().hashCode()));
+        System.out.println(xorShift(new Object().hashCode()));
+        System.out.println(xorShift(new Object().hashCode()));
+
+        Set<Integer> numbers = new HashSet<>();
+        int ITERATIONS = 20_000_000;
+        int duplicates = 0;
+        for (int i = 0; i < ITERATIONS; i++) {
+            if (!numbers.add(xorShift((int) System.nanoTime())))
+                duplicates++;
+
+            if (ITERATIONS / 2 == i)
+                System.out.println((double) duplicates * 100 / ITERATIONS);
+        }
+        System.out.println((double) duplicates * 100 / ITERATIONS);
+    }
+
+    private static int xorShift(int y) {
+        y ^= (y << 6);
+        y ^= (y >>> 21);
+        y ^= (y << 7);
+        return y;
+    }
+}
