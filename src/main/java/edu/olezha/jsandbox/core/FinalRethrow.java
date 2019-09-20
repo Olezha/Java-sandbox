00163d5abd71d12680e35ebaf402b24ac73b3445
Oleh Shklyar <oleh.shklyar@redwerk.com>
2019-09-20 19:33:32 +0300

    1

--- src/main/java/edu/olezha/jsandbox/core/FinalRethrow.java
+++ src/main/java/edu/olezha/jsandbox/core/FinalRethrow.java
@@ -4,14 +4,10 @@ public class FinalRethrow {
 
     public static void main(String[] args) {
         try {
-            try {
-                throw new ArrayIndexOutOfBoundsException();
-            } catch (Exception e) {
-                System.out.println("catch Exception");
-                throw e;
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("catch ArrayIndexOutOfBoundsException");
+            throw new ArrayIndexOutOfBoundsException();
+        } catch (Exception e) {
+            System.out.println("catch Exception");
+            throw e;
         }
     }
 }
--- src/main/java/edu/olezha/jsandbox/memory/OutOfMemoryHeapSpaceSimulator.java
+++ src/main/java/edu/olezha/jsandbox/memory/OutOfMemoryHeapSpaceSimulator.java
@@ -1,24 +1,29 @@
 package edu.olezha.jsandbox.memory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class OutOfMemoryHeapSpaceSimulator {
 
-    private static Map<String, OutOfMemoryHeapSpaceSimulator> leackContainer = new HashMap<>();
-    private static final String dataPrefix = "DATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATA";
-
     public static void main(String[] args) {
+        int a = 32;
+
         try {
-            Object lock = new Object();
-            for (int i = 0; i < 10000000; i++) {
-                String data = dataPrefix + i;
-                leackContainer.put(data, new OutOfMemoryHeapSpaceSimulator());
-                Thread.sleep(1);
-            }
-        } catch (Throwable e) {
-            System.out.println("OutOfMemoryHeapSpaceSimulator Failure: " + e);
+            System.out.println(Arrays.toString(new long[Integer.MAX_VALUE]));
+        } catch (OutOfMemoryError e) {
+            System.out.println("OutOfMemoryError: " + e.getMessage());
         }
+
+        try {
+            recursive();
+        } catch (StackOverflowError e) {
+            System.out.println("StackOverflowError");
+        }
+
+        System.out.println(a);
         System.out.println("OutOfMemoryHeapSpaceSimulator END");
+    }
+
+    private static void recursive() {
+        recursive();
     }
 }
