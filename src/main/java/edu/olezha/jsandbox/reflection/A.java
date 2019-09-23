fc7c33a76f3996dcea83f51784b100921dd1afa2
Oleh Shklyar <oleh.shklyar@redwerk.com>
2019-09-23 19:37:48 +0300

    method handles

---
+++ src/main/java/edu/olezha/jsandbox/reflection/A.java
@@ -0,0 +1,10 @@
+package edu.olezha.jsandbox.reflection;
+
+class A {
+
+    Integer sum(Integer x, Integer y) {
+        int sum = x + y;
+        System.out.print(x + " + " + y + " = ");
+        return sum;
+    }
+}
---
+++ src/main/java/edu/olezha/jsandbox/reflection/Main.java
@@ -0,0 +1,54 @@
+package edu.olezha.jsandbox.reflection;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Object obj = new A();
+
+        MethodHandle mh = getTwoArgMH();
+        try {
+            Object ret = mh.invoke(obj, 1, 1);
+            System.out.println(ret);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        // reflective
+        try {
+            System.out.println(makeReflective(obj).invoke(obj, 1, 1));
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static MethodHandle getTwoArgMH() {
+        MethodHandle mh;
+        MethodType mt = MethodType.methodType(Integer.class, Integer.class, Integer.class);
+        MethodHandles.Lookup lk = MethodHandles.lookup();
+
+        try {
+            mh = lk.findVirtual(A.class, "sum", mt);
+        } catch (NoSuchMethodException | IllegalAccessException e) {
+            throw (AssertionError) new AssertionError().initCause(e);
+        }
+
+        return mh;
+    }
+
+    private static Method makeReflective(Object obj) {
+        Method method = null;
+        try {
+            method = obj.getClass().getDeclaredMethod("sum", Integer.class, Integer.class);
+            method.setAccessible(true);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return method;
+    }
+}
