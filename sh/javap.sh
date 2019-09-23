c4640842e113fd44783a746b18b3db2544fc2ca8
Oleh Shklyar <oleh.shklyar@redwerk.com>
2019-09-23 20:04:32 +0300

    javap

---
+++ sh/javap.sh
@@ -0,0 +1,18 @@
+#javap target/classes/edu/olezha/jsandbox/core/Arr.class
+#javap -p target/classes/edu/olezha/jsandbox/core/Arr.class
+
+#javap -p -s target/classes/edu/olezha/jsandbox/core/Arr.class
+# B byte
+# C char
+# D double
+# F float
+# I int
+# J long
+# L<type_name>; Reference type
+# S short
+# Z boolean
+# [ Array
+
+#javap -v target/classes/edu/olezha/jsandbox/core/Arr.class
+
+javap -c target/classes/edu/olezha/jsandbox/core/Arr.class
