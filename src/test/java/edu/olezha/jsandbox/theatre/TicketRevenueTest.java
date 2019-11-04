45d27a86a06c6fc0ff0fe602d6d930988c57307f
Oleh Shklyar <oleh.shklyar@redwerk.com>
2019-11-04 17:06:33 +0200

    tdd red

--- src/test/java/edu/olezha/jsandbox/theatre/TicketRevenueTest.java
+++ src/test/java/edu/olezha/jsandbox/theatre/TicketRevenueTest.java
@@ -18,8 +18,29 @@ public class TicketRevenueTest {
     }
 
     @Test
-    public void oneTicketSoldIsThirtyInRevenue() {
+    public void oneTicketSoldIsTicketPriceInRevenue() {
         expectedRevenue = new BigDecimal(TicketRevenue.TICKET_PRICE);
         assertEquals(expectedRevenue, venueRevenue.estimateTotalRevenue(1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failIfLessThenZeroTicketsAreSold() {
+        venueRevenue.estimateTotalRevenue(-1);
+    }
+
+    @Test
+    public void zeroSalesEqZeroRevenur() {
+        assertEquals(BigDecimal.ZERO, venueRevenue.estimateTotalRevenue(0));
+    }
+
+    @Test
+    public void tenTicketsSoldIsTenPricesInRevenue() {
+        expectedRevenue = new BigDecimal(TicketRevenue.TICKET_PRICE * 10);
+        assertEquals(expectedRevenue, venueRevenue.estimateTotalRevenue(10));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failIfMoreThanSeatsInTheTheaterTicketsAreSold() {
+        venueRevenue.estimateTotalRevenue(101);
     }
 }
