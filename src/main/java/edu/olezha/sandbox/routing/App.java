a384069874c4163e352a38d9f7023e437c88d9de
Oleh Shklyar <oleh.shklyar@redwerk.com>
2019-11-25 16:10:29 +0200

    small world

--- src/main/java/edu/olezha/sandbox/routing/App.java
+++ src/main/java/edu/olezha/sandbox/routing/App.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 public class App {
 
-    public static final int NETWORK_SIZE = 7_000;
+    public static final int NETWORK_SIZE = 70_000;
     public static final int MAXIMUM_NUMBER_OF_PEERS = 12;
 
     public static void main(String[] args) {
@@ -18,16 +18,44 @@ public class App {
 
         Random random = new Random();
         for (Node node : nodes) {
-            for (int j = 0; j < MAXIMUM_NUMBER_OF_PEERS; j++) {
-                Node peer = nodes.get(random.nextInt(nodes.size()));
-                if (node.equals(peer)) continue;
-                node.getPeers().add(peer);
-            }
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + 1)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - 1)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + 2)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - 2)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + 3)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - 3)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + 4)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - 4)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + 6)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - 6)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + 10)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - 10)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + 50)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - 50)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + 100)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - 100)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + NETWORK_SIZE / 500)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - NETWORK_SIZE / 500)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + NETWORK_SIZE / 100)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - NETWORK_SIZE / 100)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + NETWORK_SIZE / 10)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - NETWORK_SIZE / 10)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + NETWORK_SIZE / 4)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - NETWORK_SIZE / 4)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + NETWORK_SIZE / 2)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() - NETWORK_SIZE / 2)));
+            node.getPeers().add(nodes.get(normalizeLocation(node.getLocation() + NETWORK_SIZE)));
         }
 
         Node myNode = nodes.get(random.nextInt(nodes.size()));
         for (int i = 0; i < 20; i++) {
             myNode.fetch(new Request(random.nextInt(nodes.size())));
         }
+    }
+
+    private static int normalizeLocation(int location) {
+        while (location < 0) location += NETWORK_SIZE;
+        while (location >= NETWORK_SIZE) location -= NETWORK_SIZE;
+        return location;
     }
 }
