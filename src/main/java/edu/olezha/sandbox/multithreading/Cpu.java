14eff129e6fe01b5c399082fd32928b24627db88
Oleh Shklyar <oleh.shklyar@redwerk.com>
2020-01-03 13:46:16 +0200

    available processors

---
+++ src/main/java/edu/olezha/sandbox/multithreading/Cpu.java
@@ -0,0 +1,8 @@
+package edu.olezha.sandbox.multithreading;
+
+public class Cpu {
+
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
