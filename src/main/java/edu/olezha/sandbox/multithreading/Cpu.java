f5abcb2584f4069232f2ba275f687f0e60be408c
Oleh Shklyar <oleh.shklyar@redwerk.com>
2020-01-03 13:52:05 +0200

    lazy

--- src/main/java/edu/olezha/sandbox/multithreading/Cpu.java
+++ src/main/java/edu/olezha/sandbox/multithreading/Cpu.java
@@ -2,7 +2,24 @@ package edu.olezha.sandbox.multithreading;
 
 public class Cpu {
 
+    private static int availableProcessors;
+
+    public int getAvailableProcessors() {
+        if (availableProcessors > 0) {
+            return availableProcessors;
+        }
+
+        synchronized (this) {
+            if (availableProcessors > 0) {
+                return availableProcessors;
+            }
+
+            availableProcessors = Runtime.getRuntime().availableProcessors();
+            return availableProcessors;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(Runtime.getRuntime().availableProcessors());
+        System.out.println(new Cpu().getAvailableProcessors());
     }
 }
