c614d3302ca306b162546d0b0495d8b1a35adb01
Oleh Shklyar <oleh.shklyar@redwerk.com>
2020-02-06 16:53:06 +0200

    static init

---
+++ src/main/java/edu/olezha/sandbox/core/StaticInitB.java
@@ -0,0 +1,8 @@
+package edu.olezha.sandbox.core;
+
+public class StaticInitB {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        System.out.println("B A.A: " + StaticInitA.A);
+    }
+}
