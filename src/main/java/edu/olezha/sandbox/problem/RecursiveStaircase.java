709bb228e4d90bd8e82444b45add0da07e4168e7
Oleh Shklyar <oleh.shklyar@gmail.com>
2020-02-08 11:51:11 +0200

    generalization

--- src/main/java/edu/olezha/sandbox/problem/RecursiveStaircase.java
+++ src/main/java/edu/olezha/sandbox/problem/RecursiveStaircase.java
@@ -2,23 +2,19 @@ package edu.olezha.sandbox.problem;
 
 public class RecursiveStaircase {
 
-    static int numWays(int N) {
-        if (N < 0) throw new IllegalArgumentException();
+    static int numWays(int N, int[] possibleSteps) {
+        if (N < 0) return 0;
         if (N < 2) return 1;
 
-        int last = 1;
-        int oneBeforeLast = 1;
+        int numWays = 0;
+        for (int possibleStep : possibleSteps)
+            numWays += numWays(N - possibleStep, possibleSteps);
 
-        for (int i = 2; i <= N; i++) {
-            int next = last + oneBeforeLast;
-            oneBeforeLast = last;
-            last = next;
-        }
-
-        return last;
+        return numWays;
     }
 
     public static void main(String[] args) {
-        System.out.println(numWays(4));
+        System.out.println(numWays(4, new int[] {1, 2}));
+        System.out.println(numWays(4, new int[] {1, 3, 5}));
     }
 }
