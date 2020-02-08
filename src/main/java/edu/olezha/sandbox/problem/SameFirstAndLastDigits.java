7f48437b87030a5cf20cc8e24ac1dc7eb9c54e29
Oleh Shklyar <oleh.shklyar@gmail.com>
2020-02-08 14:52:54 +0200

    optimization comment

--- src/main/java/edu/olezha/sandbox/problem/SameFirstAndLastDigits.java
+++ src/main/java/edu/olezha/sandbox/problem/SameFirstAndLastDigits.java
@@ -3,11 +3,17 @@ package edu.olezha.sandbox.problem;
 /**
  * Given an range of numbers count the numbers which have same first and last digits
  * Ex. Between 7 and 20 such numbers are 8, 9 and 11
+ *
+ * 1-100: 9+9
+ * 100-200: 10
+ * 1_000-2_000: 100
+ * 10_000-20_000: 1_000
  */
 public class SameFirstAndLastDigits {
 
     public static void main(String[] args) {
         System.out.println(countNumbersWithSameDigits(7, 20));
+        System.out.println(countNumbersWithSameDigits(3, 441));
     }
 
     // O(n)
