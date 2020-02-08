2823fd7c9631a76d499cefedb86e3ec26ee4f142
Oleh Shklyar <oleh.shklyar@gmail.com>
2020-02-08 12:07:07 +0200

    dynamic programming

--- src/main/java/edu/olezha/sandbox/problem/RecursiveStaircase.java
+++ src/main/java/edu/olezha/sandbox/problem/RecursiveStaircase.java
@@ -3,18 +3,22 @@ package edu.olezha.sandbox.problem;
 public class RecursiveStaircase {
 
     static int numWays(int N, int[] possibleSteps) {
-        if (N < 0) return 0;
-        if (N < 2) return 1;
+        if (N < 0) throw new IllegalArgumentException();
 
-        int numWays = 0;
-        for (int possibleStep : possibleSteps)
-            numWays += numWays(N - possibleStep, possibleSteps);
+        int[] numWays = new int[N + 1];
+        numWays[0] = 1;
+        for (int i = 1; i < numWays.length; i++)
+            for (int possibleStep : possibleSteps)
+                if (i - possibleStep >= 0)
+                    numWays[i] += numWays[i - possibleStep];
 
-        return numWays;
+        return numWays[N];
     }
 
     public static void main(String[] args) {
         System.out.println(numWays(4, new int[] {1, 2}));
         System.out.println(numWays(4, new int[] {1, 3, 5}));
+        System.out.println(numWays(2, new int[] {}));
+        System.out.println(numWays(1, new int[] {}));
     }
 }
