88869e67a25083a4875dbc3cf515123665493fc5
Oleh Shklyar <oleh.shklyar@redwerk.com>
2020-02-10 20:17:50 +0200

    2

--- src/main/java/edu/olezha/sandbox/problem/UniqueIntegerInArray.java
+++ src/main/java/edu/olezha/sandbox/problem/UniqueIntegerInArray.java
@@ -1,6 +1,8 @@
 package edu.olezha.sandbox.problem;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Given the array of IDs, which contains
@@ -10,18 +12,30 @@ import java.util.Arrays;
 public class UniqueIntegerInArray {
 
     public static void main(String[] args) {
-        System.out.println(unique(new int[] {1, 2, 3, 4, 5, 6, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9})); // 7
+        System.out.println(unique2(new int[] {1, 2, 3, 4, 5, 6, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9})); // 7
+        System.out.println(unique2(new int[] {1, 2, 3, 4, 1, 2, 4})); // 3
+        System.out.println(unique2(new int[] {100, 33, 27, 89, 27, 99, 33, 99, 100})); // 89
     }
 
     // O(NlgN)
     static int unique(int[] a) {
         Arrays.sort(a);
-        System.out.println(Arrays.toString(a));
         for (int i = 0; i < a.length - 1; i += 2) {
             if (a[i] != a[i + 1]) {
                 return a[i];
             }
         }
         throw new IllegalStateException();
+    }
+
+    // O(N)?
+    static int unique2(int[] a) {
+        Set<Integer> known = new HashSet<>();
+        for (int e : a) {
+            if (known.contains(e)) known.remove(e);
+            else known.add(e);
+        }
+        if (known.size() != 1) throw new IllegalStateException();
+        return (int) known.toArray()[0];
     }
 }
