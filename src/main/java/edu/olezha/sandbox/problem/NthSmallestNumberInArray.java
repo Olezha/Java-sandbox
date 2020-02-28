1b1d291069ce2b796adfeed4ff41ad26130797bf
Oleh Shklyar <oleh.shklyar@redwerk.com>
2020-02-28 17:58:10 +0200

    modify to sort subarray first

--- src/main/java/edu/olezha/sandbox/problem/NthSmallestNumberInArray.java
+++ src/main/java/edu/olezha/sandbox/problem/NthSmallestNumberInArray.java
@@ -11,16 +11,18 @@ public class NthSmallestNumberInArray {
         int n = 4;
         int[] arr = {13, 7, 6, 45, 21, 9, 2, 100};
 
-        int[] nArr = new int[n];
-        for (int i = 0; i < n; i++) nArr[i] = arr[0];
-        for (int v : arr) {
-            if (v < nArr[n - 1]) {
-                nArr[n - 1] = v;
-                for (int i = n - 1; i > 0; i--) {
-                    if (nArr[i] < nArr[i - 1]) {
-                        int x = nArr[i];
-                        nArr[i] = nArr[i - 1];
-                        nArr[i - 1] = x;
+        int[] nArr = Arrays.copyOf(arr, n);
+        Arrays.sort(nArr);
+        for (int i = n; i < arr.length; i++) {
+            if (arr[i] < nArr[n - 1]) {
+                nArr[n - 1] = arr[i];
+                for (int j = n - 1; j > 0; j--) {
+                    if (nArr[j] < nArr[j - 1]) {
+                        int tmp = nArr[j];
+                        nArr[j] = nArr[j - 1];
+                        nArr[j - 1] = tmp;
+                    } else {
+                        break;
                     }
                 }
             }
