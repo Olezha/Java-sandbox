c86fb596de9f5413b9faf45d690fd3e96698ec0d
Oleh Shklyar <oleh.shklyar@redwerk.com>
2020-02-28 16:41:13 +0200

    O(n)

--- src/main/java/edu/olezha/sandbox/problem/NthSmallestNumberInArray.java
+++ src/main/java/edu/olezha/sandbox/problem/NthSmallestNumberInArray.java
@@ -10,8 +10,22 @@ public class NthSmallestNumberInArray {
     public static void main(String[] args) {
         int n = 4;
         int[] arr = {13, 7, 6, 45, 21, 9, 2, 100};
-        Arrays.sort(arr);
 
-        System.out.printf("n-th element: %d", arr[n-1]);
+        int[] nArr = new int[n];
+        for (int i = 0; i < n; i++) nArr[i] = arr[0];
+        for (int v : arr) {
+            if (v < nArr[n - 1]) {
+                nArr[n - 1] = v;
+                for (int i = n - 1; i > 0; i--) {
+                    if (nArr[i] < nArr[i - 1]) {
+                        int x = nArr[i];
+                        nArr[i] = nArr[i - 1];
+                        nArr[i - 1] = x;
+                    }
+                }
+            }
+        }
+
+        System.out.printf("n-th element: %d", nArr[n - 1]);
     }
 }
