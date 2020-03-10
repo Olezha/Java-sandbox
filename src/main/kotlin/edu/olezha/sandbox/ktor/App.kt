407a1bf5d49f77200c641ab860031271df85db82
Oleh Shklyar <oleh.shklyar@redwerk.com>
2020-03-10 20:51:59 +0200

    json response

--- src/main/kotlin/edu/olezha/sandbox/ktor/App.kt
+++ src/main/kotlin/edu/olezha/sandbox/ktor/App.kt
@@ -1,8 +1,10 @@
 package edu.olezha.sandbox.ktor
 
 import io.ktor.application.call
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
@@ -10,10 +12,16 @@ import io.ktor.server.netty.Netty
  
 fun main() {
     embeddedServer(Netty, 8080) {
+        install(ContentNegotiation) {
+            jackson {
+            }
+        }
         routing {
             get("/") {
-                call.respondText("Hi", ContentType.Text.Html)
+                call.respond(Person("Oleh"))
             }
         }
     }.start(true)
 }
+
+data class Person(val name: String)
