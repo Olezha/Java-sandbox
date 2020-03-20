4b656e66e63d53edcbfdc9413f93efb3c0aedbce
Oleh Shklyar <oleh.shklyar@gmail.com>
2020-03-20 13:13:31 +0200

    360

--- src/main/kotlin/edu/olezha/sandbox/Extensions.kt
+++ src/main/kotlin/edu/olezha/sandbox/Extensions.kt
@@ -1,13 +1,25 @@
 package edu.olezha.sandbox
 
-fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
+infix fun String.addEnthusiasm(amount: Int) = this + "!".repeat(amount)
+
+val String.numVowels
+    get() = count { "aeiouy".contains(it) }
 
 fun <T> T.out(): T {
     println(this)
     return this
 }
 
+infix fun String?.printWithDefault(default: String) = println(this ?: default)
+
 fun main() {
-    "Hi".out().addEnthusiasm(5).out()
+    "Hi".out()
+            .addEnthusiasm(5).out()
+            .numVowels.out()
     36.out().out()
+
+    val nullableString: String? = null
+    nullableString printWithDefault "Default string"
+
+    println("Hi" addEnthusiasm 3)
 }
