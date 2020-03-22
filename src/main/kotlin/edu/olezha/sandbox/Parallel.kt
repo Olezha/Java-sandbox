02ce863082b57a1f8ef69533431d4da1f8da35e9
Oleh Shklyar <oleh.shklyar@gmail.com>
2020-03-22 20:34:51 +0200

    async

--- src/main/kotlin/edu/olezha/sandbox/Parallel.kt
+++ src/main/kotlin/edu/olezha/sandbox/Parallel.kt
@@ -1,19 +1,30 @@
 package edu.olezha.sandbox
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 fun main() {
     GlobalScope.launch {
+        fetchAsync(3).await().out()
+        fetchAsync(1).await().out()
+        fetchAsync(3).await().out()
+    }
+
+    GlobalScope.launch {
         repeat(10) {
             delay(400)
-            "Async $it".out()
+            "Launch $it".out()
         }
     }
 
     repeat(5) {
         Thread.sleep(1_000)
         "Main $it".out()
+    }
+}
+
+fun fetchAsync(sec: Long): Deferred<String> {
+    return GlobalScope.async {
+        delay(sec * 1_000)
+        "Async ok $sec"
     }
 }
